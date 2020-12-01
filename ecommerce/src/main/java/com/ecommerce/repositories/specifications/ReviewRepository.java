0b146c058bdf1ca9634c67c1e977ef61d32830a3
Ciupix22 <ciprian.teletin99@e-uvt.ro>
2020-12-01 19:21:31 +0200

    ReviewRepository modified

--- ecommerce/src/main/java/com/ecommerce/repositories/specifications/ReviewRepository.java
+++ ecommerce/src/main/java/com/ecommerce/repositories/specifications/ReviewRepository.java
@@ -2,17 +2,12 @@ package com.ecommerce.repositories.specifications;
 
 import com.ecommerce.models.Review;
 
-import java.util.List;
-
-public interface ReviewRepository {
+public interface ReviewRepository extends RepositoryManager<Review, Long>{
 
     void save(Review review, Long idProduct, Long idUser);
-    void delete(Review review);
-    void deleteById(Long id);
-    int count();
-    List<Review> findAll();
-    boolean existsById(Long id);
-    Review findById(Long id);
 
-
+    @Override
+    default <S extends Review> S save(S entity) {
+        return null;
+    }
 }
